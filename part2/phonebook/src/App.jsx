dc211c8378a6f7c731b8146ef6ee696a9550d619
asija anand <asija.anand.official@gmail.com>
2026-07-19 12:20:08 +0530

    till q2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas' },
-        { name: 'Ada Lovelace' }
+        { 
+            name: 'Arto Hellas',
+            number: '00-99999-99999'
+        },
     ]) 
     const [newName, setNewName] = useState('Add Number')
+    const [newNumber, setNewNumber] = useState('00-00000-00000')
 
     const check = () => {
         for(let i = 0; i < persons.length; i++){
-            if(newName === persons[i].name) return true
+            if(newName === persons[i].name && newNumber === persons[i].number) return true
         }
         return false
     }
@@ -19,13 +22,20 @@ const App = () => {
         if(check()) alert(`'${newName}' is already added to phonebook`)
         else{
             setPersons(persons.concat(
-                {name: newName}
+                {
+                    name: newName,
+                    number: newNumber
+                }
             ))
         }
         setNewName('Add Number')
     }
-    const handleOnChange = (event) => {
+    const handleOnName = (event) => {
         setNewName(event.target.value)
+    }
+
+    const handleOnNumber = (event) => {
+        setNewNumber(event.target.value)
     }
 
     return (
@@ -33,7 +43,10 @@ const App = () => {
             <h2>Phonebook</h2>
             <form onSubmit={handleSubmit}>
                 <div>
-                    name: <input value={newName} onChange={handleOnChange}/>
+                    name: <input value={newName} onChange={handleOnName}/>
+                </div>
+                <div>
+                    number: <input value={newNumber} onChange={handleOnNumber}/>
                 </div>
 
                 <div>
@@ -44,8 +57,8 @@ const App = () => {
             <h2>Numbers</h2>
             {persons.map(
                 person =>
-                <p key={person.name}>
-                    {person.name}
+                <p key={person.number + person.name}>
+                    {person.name} {person.number}
                 </p>
             )}
         </div>
