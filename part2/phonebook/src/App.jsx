40499e4bba558d5269646a8e3747907115519b81
asija anand <asija.anand.official@gmail.com>
2026-07-19 11:14:05 +0530

    till q2.6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,1 +1,45 @@
 import { useState } from 'react'
+
+const App = () => {
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas' },
+        { name: 'Ada Lovelace' }
+    ]) 
+    const [newName, setNewName] = useState('Add Numbers')
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        setPersons(persons.concat(
+            {name: newName}
+        ))
+        setNewName('Add Number')
+    }
+    const handleOnChange = (event) => {
+        setNewName(event.target.value)
+    }
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    name: <input value={newName} onChange={handleOnChange}/>
+                </div>
+
+                <div>
+                <button type="submit">add</button>
+                </div>
+            </form>
+            <br />
+            <h2>Numbers</h2>
+            {persons.map(
+                person =>
+                <p key={person.name}>
+                    {person.name}
+                </p>
+            )}
+        </div>
+    )
+}
+
+export default App
